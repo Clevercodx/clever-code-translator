84f3860dde126f1e5643eff1085a9fbe381efe2c
Clevercodex <115954304+Clevercodx@users.noreply.github.com>
2023-04-07 10:14:14 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -130,7 +130,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Code Translator</title>
+        <title>Clever Code Translator</title>
         <meta
           name="description"
           content="Use AI to translate code from one language to another."
@@ -139,13 +139,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex h-full min-h-screen flex-col items-center bg-[#0E1117] px-4 pb-20 text-neutral-200 sm:px-10">
-        <div className="mt-10 flex flex-col items-center justify-center sm:mt-20">
-          <div className="text-4xl font-bold">AI Code Translator</div>
+        <div className="mt-20 flex flex-col items-center justify-center sm:mt-30">
+          <div className="text-4xl font-bold">Clever Code Translator</div>
         </div>
 
-        <div className="mt-6 text-center text-sm">
-          <APIKeyInput apiKey={apiKey} onChange={handleApiKeyChange} />
-        </div>
+        
 
         <div className="mt-2 flex items-center space-x-2">
           <ModelSelect model={model} onChange={(value) => setModel(value)} />
